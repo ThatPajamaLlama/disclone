16a93ef797003afab5307f6a63726d318d22a8de
Nozzergirl <shannon_norris@hotmail.co.uk>
2021-10-17 20:28:37 +0100

    Login Beginnings

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <?php include "assets/inc/head.php"; ?>
+    <body id="login">
+        <div id="content">
+            <form class="glass-card" action="assets/proc/login_process.php" method="POST">
+                <div>
+                    <p>test</p>
+                    <p>test</p>
+                    <p>test</p>
+                    <p>test</p>
+
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
