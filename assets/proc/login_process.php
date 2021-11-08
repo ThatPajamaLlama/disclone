9f792627a10ccddee18df5a7b028dccb7c26e121
Nozzergirl <shannon_norris@hotmail.co.uk>
2021-11-08 19:26:57 +0000

    Login/signup

--- assets/proc/login_process.php
+++ assets/proc/login_process.php
@@ -3,9 +3,16 @@
 include "../inc/database_connection.php";
 
 $username = $_POST['username'];
+$username = mysqli_real_escape_string($conn, $username);
+
 $password = $_POST['password'];
 
 $sql_login = "SELECT password FROM user WHERE username='$username'";
+$rs_login = mysqli_query($conn, $sql_login);
 
-
-?>
+if (mysqli_num_rows($rs_login) == 1) {
+    $details = mysqli_fetch_assoc($rs_login);
+    if (password_verify($password, $details['password'])) {
+        header('location: ../../chat.php');
+    }
+}
---
+++ error.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<?php include "assets/inc/head.php"; ?>
+
+<body id="error" class="center-contents form-page">
+    <div id="content">
+        <div class="glass-card">
+            <h1>Error</h1>
+            <h2>This is an issue</h2>
+        </div>
+    </div>
+</body>
+
+</html>
